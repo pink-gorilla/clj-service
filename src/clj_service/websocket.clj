7099caf2ffdf68be2340d91ff67c0841f6cd49fb
awb99 <awb99@gmail.com>
2026-02-25 20:38:33 -0500

    use oauth2 tokens

--- src/clj_service/websocket.clj
+++ src/clj_service/websocket.clj
@@ -2,10 +2,9 @@
   (:require
    [clojure.string]
    [de.otto.nom.core :as nom]
-   [taoensso.timbre :as timbre :refer [error]]
-   [modular.permission.session :refer [get-user]]
+   [taoensso.timbre :as timbre :refer [warn error]]
    [modular.ws.msg-handler :refer [-event-msg-handler send-response]]
-   [clj-service.executor :refer [execute-with-binding *user*]]))
+   [clj-service.executor :refer [execute-with-binding]]))
 
 (defn error-response [user fun args r]
   (error "clj-service websocket execution error: " r)
@@ -17,14 +16,17 @@
 (defn response [r]
   {:result r})
 
-(defn create-websocket-responder [{:keys [permission-service] :as this}]
+(defn create-websocket-responder [this]
   (defmethod -event-msg-handler :clj/service
-    [{:keys [event _id _?data uid] :as req}]
+    [{:keys [event _id _?data uid client-id uid] :as req}]
+    ;(warn "websocket req keys: " (keys req))
+    (warn "websocket browser-client-id: " client-id " uid:" uid)
+    ; client-id is browser-session id
+    ; uid is our username
     (let [[_ params] event ; _ is :clj/service
-          {:keys [fun args]} params
-          user (get-user permission-service uid)]
+          {:keys [fun args]} params]
       (future
-        (let [r (execute-with-binding this user uid fun args)]
+        (let [r (execute-with-binding this uid client-id fun args)]
           (if (nom/anomaly? r)
-            (send-response req :clj/service (error-response user fun args r))
+            (send-response req :clj/service (error-response uid fun args r))
             (send-response req :clj/service (response r))))))))
